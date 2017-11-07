086d857f5b8e13686b761ed87dd7c49cd72c7495
Stas <sromanenko91@gmail.com>
2017-11-07 19:41:13 +0300

    Теперь отправляет только новые записи

--- parsim_freelansim.php
+++ parsim_freelansim.php
@@ -4,29 +4,48 @@ $dom_xml = new DomDocument(2.0, 'UTF-8');
 $dom_xml->load('https://freelansim.ru/user_rss_tasks/6Fpi1p32eMAPheTrxdyh');
 
 $items=$dom_xml->getElementsByTagName("item");
-$date = $items->item(0)->getElementsByTagName("pubDate");
 
-$pattern = '/(<br>)+/';
+$patternForRegexp = '/(<br>)+/';  // шаблон для замены <br> на \n в description
+$patternForXML = ( file_exists('.lastLink') ) ? file_get_contents('.lastLink') : null;
 
-$arrData = []; //
+$count = 0;
 foreach ($items as $item )
 {
     $date = $item->getElementsByTagName("pubDate");
-    $date = strip_tags(trim($date[0]->nodeValue),'<a>');
+    $date = $date[0]->nodeValue;
 
     $title = $item->getElementsByTagName("title");
-    $title = strip_tags(trim($title[0]->nodeValue),'<a>');
+    $title = strip_tags( trim($title[0]->nodeValue),'<a>' );
 
     $link = $item->getElementsByTagName("link");
-    $link = strip_tags(trim($link[0]->nodeValue),'<a>');
+    $link = $link[0]->nodeValue;
 
     $description = $item->getElementsByTagName("description");
-    $description = strip_tags( preg_replace( $pattern, "\n", trim( $description[0]->nodeValue ) ),'<a>' );
-    //$arrData[] = compact('date', 'title', 'link', 'description' );
+    $description = strip_tags( preg_replace( $patternForRegexp, "\n", trim( $description[0]->nodeValue ) ),'<a>' );
+
+    if ($link === $patternForXML)
+    {
+        if ( $count !== 0 )
+        {
+            $link = $items->item(0)->getElementsByTagName("link")->item(0)->nodeValue;
+            $f = fopen('.lastLink', 'w');
+            fwrite($f, $link);
+            fclose($f);
+        }
+        break;
+    }
+
     $str = "<b>" .$date . "</b>\n<b>" . $title . "</b>\n" . $description . "\n" . $link;
     $str = urlencode($str);
-
     $query = "https://api.telegram.org/bot$token/$method?$options&text=$str";
     file($query);
-    break;
+    $count++;
+}
+
+//если файла для сравнения нет, или с не было совпадений с ним
+if ( $items->length == $count){
+    $link = $items->item(0)->getElementsByTagName("link")->item(0)->nodeValue;
+    $f = fopen('.lastLink', 'w');
+    fwrite($f, $link);
+    fclose($f);
 }

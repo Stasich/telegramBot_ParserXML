bff29df96e1677bcf72254a3f7094a38491d8890
Stas <sromanenko91@gmail.com>
2017-11-07 19:51:11 +0300

    выделил chat_id в отдельную переменную

--- settings_for_api.php
+++ settings_for_api.php
@@ -1,4 +1,5 @@
 <?php
 $token = 'Your Token';
 $method = 'sendMessage';
-$options = 'chat_id=@parsim_freelansim&disable_web_page_preview=true&parse_mode=html';
+$chat_id = '@parsim_freelansim';
+$options = "chat_id=$chat_id&disable_web_page_preview=true&parse_mode=html";
